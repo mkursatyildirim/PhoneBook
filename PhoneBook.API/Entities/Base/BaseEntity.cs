eab95cdddbff1d8d8eb3dc02f827827c2bc20d02
mkursatyildirim <m.kursatyildirim33@gmail.com>
2021-12-18 02:14:52 +0300

    EntityFramework için Base yapı ve Context oluşturuldu. Database bağlantısı için gerekli konfigürasyonlar yapıldı.

---
+++ PhoneBook.API/Entities/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.API.Entities.Base
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ PhoneBook.API/Entities/Context/PhoneBookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhoneBook.API.Entities.Context
+{
+    public class PhoneBookContext : DbContext
+    {
+        public PhoneBookContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected PhoneBookContext()
+        {
+        }
+    }
+}
