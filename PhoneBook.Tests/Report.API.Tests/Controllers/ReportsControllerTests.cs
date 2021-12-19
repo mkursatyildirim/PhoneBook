e3b39202c57cfcaba3fd0e86dc259487ec6c7211
mkursatyildirim <m.kursatyildirim33@gmail.com>
2021-12-19 06:17:49 +0300

    ReportsController'da bulunan GetReportDetail metodu için unit test yazıldı.

--- PhoneBook.Tests/Report.API.Tests/Controllers/ReportsControllerTests.cs
+++ PhoneBook.Tests/Report.API.Tests/Controllers/ReportsControllerTests.cs
@@ -1,7 +1,10 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using PhoneBook.Tests.Helpers;
 using Report.API.Controllers;
+using Report.API.Dto;
 using Report.API.Services;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -14,9 +17,9 @@ namespace PhoneBook.Tests.Report.API.Tests.Controllers
         {
             var mockReportService = new Mock<IReportService>();
 
-            var personsController = new ReportsController(mockReportService.Object);
+            var reportsController = new ReportsController(mockReportService.Object);
 
-            var result = await personsController.ReportRequest();
+            var result = await reportsController.ReportRequest();
 
             Assert.Equal(200, TestHelper.GetStatusCodeFromActionResult(result));
         }
@@ -26,11 +29,54 @@ namespace PhoneBook.Tests.Report.API.Tests.Controllers
         {
             var mockReportService = new Mock<IReportService>();
 
-            var personsController = new ReportsController(mockReportService.Object);
+            var reportsController = new ReportsController(mockReportService.Object);
 
-            var result = await personsController.GetAllReports();
+            var result = await reportsController.GetAllReports();
 
             Assert.Equal(200, TestHelper.GetStatusCodeFromActionResult(result));
+        }
+
+        [Fact]
+        public async Task GetReportDetail_With_Valid_Params_Should_Return_200()
+        {
+
+            var reportId = Guid.NewGuid();
+            var mockReportService = new Mock<IReportService>();
+            mockReportService
+                .Setup(x => x.GetReportDetail(reportId))
+                .ReturnsAsync(() => new ReportDetailDto()
+                {
+                    Report = new ReportDto()
+                    {
+                        UUID = reportId
+                    }
+                });
+                
+
+            var reportsController = new ReportsController(mockReportService.Object);
+
+            var result = await reportsController.GetReportDetail(reportId);
+
+            Assert.Equal(200, TestHelper.GetStatusCodeFromActionResult(result));
+        }
+
+        [Fact]
+        public async Task GetReportDetail_With_Invalid_Params_Should_Return_404()
+        {
+
+            var mockReportService = new Mock<IReportService>();
+            mockReportService
+                .Setup(x => x.GetReportDetail(It.IsAny<Guid>()))
+                .ReturnsAsync(() => null);
+
+
+            var reportsController = new ReportsController(mockReportService.Object);
+
+            var result = await reportsController.GetReportDetail(Guid.Empty);
+
+            var notFoundResult = Assert.IsType<NotFoundResult>(result.Result);
+
+            Assert.Equal(404, notFoundResult.StatusCode);
         }
     }
 }
