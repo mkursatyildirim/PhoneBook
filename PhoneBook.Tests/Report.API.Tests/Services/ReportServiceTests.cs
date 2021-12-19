b231f4b63e8858c85871ce28824ee38550c68c2c
mkursatyildirim <m.kursatyildirim33@gmail.com>
2021-12-19 19:54:33 +0300

    GenerateStatisticReport için hata durumunu kontrol eden unit test yazıldı.

--- PhoneBook.Tests/Report.API.Tests/Services/ReportServiceTests.cs
+++ PhoneBook.Tests/Report.API.Tests/Services/ReportServiceTests.cs
@@ -1,8 +1,17 @@
-﻿using PhoneBook.Tests.Helpers;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Moq.Protected;
+using PhoneBook.Tests.Helpers;
+using Report.API.Constants;
 using Report.API.Entities.Context;
 using Report.API.Enums;
 using Report.API.Services;
 using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 using Entities = Report.API.Entities;
@@ -75,6 +84,39 @@ namespace PhoneBook.Tests.Report.API.Tests.Services
 
             Assert.NotNull(result.Report);
             Assert.NotNull(result.ReportDetails);
+        }
+
+        [Fact]
+        public async Task GenerateStatisticsReport_If_Report_Not_Found_Should_Throw_Error()
+        {
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("[{'uuid': 'a5ba5d9c-f6fa-4466-9ef6-508592438fc9','informationType':2,'informationContent':'Mersin', 'personId': 'b890413c-2dca-499a-8578-5cfb14e0b1eb'}]"),
+                });
+
+            var mockFactory = new Mock<IHttpClientFactory>();
+
+            var client = new HttpClient(mockHttpMessageHandler.Object);
+            mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+            IOptions<ReportSettings> settings = Options.Create<ReportSettings>(new ReportSettings()
+            {
+                PhoneBookApiUrl = "test_url"
+            });
+
+            
+            var serviceCollection = new ServiceCollection().AddLogging();
+            var logger = serviceCollection.BuildServiceProvider().GetService<ILogger<ReportService>>();
+
+            var context = new ReportContext(TestHelper.GetReportContextForInMemoryDb());
+
+            var service = new ReportService(context, mockFactory.Object, logger, settings);
+
+            Func<Task> func = () =>  service.GenerateStatisticsReport(Guid.Parse("a5ba5d9c-f6fa-4466-9ef6-508592438fc9"));             //assert
+            Exception exception = await Assert.ThrowsAsync<Exception>(func);
         }
     }
 }
