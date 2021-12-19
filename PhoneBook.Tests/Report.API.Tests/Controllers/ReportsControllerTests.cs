3ebb124defe0c9cda18286bf976ac41c83992579
mkursatyildirim <m.kursatyildirim33@gmail.com>
2021-12-19 06:06:36 +0300

    Report.API projesi için ReportsControllerTests oluşturuldu ve ReportRequest metodu için unit test yazıldı.

---
+++ PhoneBook.Tests/Report.API.Tests/Controllers/ReportsControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using PhoneBook.Tests.Helpers;
+using Report.API.Controllers;
+using Report.API.Services;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PhoneBook.Tests.Report.API.Tests.Controllers
+{
+    public class ReportsControllerTests
+    {
+        [Fact]
+        public async Task ReportRequest_Should_Return_200()
+        {
+            var mockReportService = new Mock<IReportService>();
+
+            var personsController = new ReportsController(mockReportService.Object);
+
+            var result = await personsController.ReportRequest();
+
+            Assert.Equal(200, TestHelper.GetStatusCodeFromActionResult(result));
+        }
+    }
+}
