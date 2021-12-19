863087be2354a36db9daa009e01d38c7acb9c7af
mkursatyildirim <m.kursatyildirim33@gmail.com>
2021-12-19 03:33:11 +0300

    Gerekli NuGet paketleri yüklendi ve TestHelper classı yazıldı.

---
+++ PhoneBook.Tests/Helpers/TestHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhoneBook.API.Entities.Context;
+using Report.API.Entities.Context;
+using System;
+
+namespace PhoneBook.Tests.Helpers
+{
+    public static class TestHelper
+    {
+        public static DbContextOptions<PhoneBookContext> GetPhoneBookContextForInMemoryDb()
+        {
+            return new DbContextOptionsBuilder<PhoneBookContext>()
+                .UseInMemoryDatabase(databaseName: "PhoneBook" + DateTime.Now)
+                .Options;
+        }
+
+        public static DbContextOptions<ReportContext> GetReportContextForInMemoryDb()
+        {
+            return new DbContextOptionsBuilder<ReportContext>()
+                .UseInMemoryDatabase(databaseName: "Report" + DateTime.Now)
+                .Options;
+        }
+
+        public static int? GetStatusCodeFromActionResult<T>(ActionResult<T> actionResult)
+            => ((ObjectResult)actionResult.Result).StatusCode;
+    }
+}
